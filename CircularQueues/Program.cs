05eeee9e8f091fa35ba08cb54029c64a820d30e5
adrianrezha29 <adrianrezha29@gmail.com>
2022-12-16 17:42:08 +0700

    add class queues

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CircularQueues
 {
-    internal class Program
+    class Queues
     {
         static void Main(string[] args)
         {
